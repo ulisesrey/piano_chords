1ffbd9fa972a47ef3b7b01df3b92a5e8a8a281c6
ulisesrey <ulises.rey.torne@gmail.com>
2026-01-05 15:07:38 +0100

    Fix https://github.com/ulisesrey/piano_chords/issues/16

--- piano_chords/chord_generator.py
+++ piano_chords/chord_generator.py
@@ -2,12 +2,20 @@ class Chord:
     NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 
     FORMULAS = {
+        # Triads
         "Major": [0, 4, 7],
         "Minor": [0, 3, 7],
         "Diminished": [0, 3, 6],
         "Augmented": [0, 4, 8],
+
+        # Seventh chords (core jazz harmony)
         "Major 7": [0, 4, 7, 11],
+        "Dominant 7": [0, 4, 7, 10],
+        "Minor 7": [0, 3, 7, 10],
+        "Half-diminished 7": [0, 3, 6, 10],
+        "Diminished 7": [0, 3, 6, 9],
     }
+
 
     SYMBOLS = {
         "Major": "",
@@ -15,6 +23,10 @@ class Chord:
         "Diminished": "°",
         "Augmented": "+",
         "Major 7": "maj7",
+        "Dominant 7": "7",
+        "Minor 7": "m7",
+        "Half-diminished 7": "ø7",
+        "Diminished 7": "°7",
     }
 
     def __init__(self, root, quality):
